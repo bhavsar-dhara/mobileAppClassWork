86a0a3ce1f57e9b87c170ae266209019c131c745
Dhara Bhavsar <dharapb@ccs.neu.edu>
2016-03-19 01:24:05 -0400

    Committing code that will restrict the register and unregister notification from coming

--- NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/gcmcomm/GcmBroadcastReceiver.java
+++ NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/gcmcomm/GcmBroadcastReceiver.java
@@ -9,9 +9,11 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		ComponentName comp = new ComponentName(context.getPackageName(),
-				GcmIntentService.class.getName());
-		startWakefulService(context, (intent.setComponent(comp)));
+		if(CommunicationConstants.alertText.equals("Message Notification")) {
+			ComponentName comp = new ComponentName(context.getPackageName(),
+					GcmIntentService.class.getName());
+			startWakefulService(context, (intent.setComponent(comp)));
+		}
 		setResultCode(Activity.RESULT_OK);
 	}
 }
