2f31fc45d99d90f962113827a3c6f3c961639ef8
Dhara Bhavsar <dbhavsar@massmutual.com>
2016-11-29 11:15:22 -0500

    Displaying proper app action bar in  about screen

--- NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/about/MainActivityAbout.java
+++ NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/about/MainActivityAbout.java
@@ -1,20 +1,22 @@
 package edu.neu.madcourse.dharabhavsar.ui.about;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import edu.neu.madcourse.dharabhavsar.ui.main.R;
-
-//import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by Dhara on 1/22/2016.
  */
-public class MainActivityAbout extends FragmentActivity {
+public class MainActivityAbout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.about_app_screen));
     }
+
 }
