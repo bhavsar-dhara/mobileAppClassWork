60ce6a996a8f1d90d4b1f2c1d6fcf56368f6d106
Dhara Bhavsar <dbhavsar@massmutual.com>
2016-12-02 10:08:53 -0500

    Added activity transition between main <-> about activities

--- NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/about/MainActivityAbout.java
+++ NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/about/MainActivityAbout.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.dharabhavsar.ui.about;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import edu.neu.madcourse.dharabhavsar.ui.main.R;
 
@@ -17,6 +18,26 @@ public class MainActivityAbout extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.about_app_screen));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/dictionary/MainActivityDict.java
+++ NUMAD16S-DharaBhavsar/NUMAD16S-DharaBhavsar/app/src/main/java/edu/neu/madcourse/dharabhavsar/ui/dictionary/MainActivityDict.java
@@ -212,26 +212,6 @@ public class MainActivityDict extends AppCompatActivity {
         Log.d("MainActivityDict", "state = " + gameData);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            finish();
-            overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     //    getData()
 
     /**
